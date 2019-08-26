ac6d964568bf6276cb6cfcf775b2d3117712721d
mojianhua <1538964080@qq.com>
2019-08-26 15:47:37 +0800

    class完成

--- newStudyPython/规范化/lib/classStudy.py
+++ newStudyPython/规范化/lib/classStudy.py
@@ -1,4 +1,5 @@
 import random
+from math import pi
 class Persion:
     # 静态属性
     country = 'China'
@@ -38,7 +39,7 @@ class Persion2:
                 print('%s被 %s 击败了' % (dog.name,self.name))
                 exit()
 
-class Dog():
+class Dog:
     def __init__(self,*args):
         self.name = args[0]
         self.hp = args[1]
@@ -57,3 +58,45 @@ class Dog():
             if (int(self.hp) == 0 or int(self.hp) < 0):
                 print('%s 被 %s 击败了' % (persion.name,self.name))
                 exit()
+
+# 圆类
+class Circle:
+    def __init__(self,r):
+        self.r = r
+
+    def area(self):
+        return self.r**2 * pi
+
+    def perimeter(self):
+        return 2*pi*self.r
+
+# 圆环
+class Ring:
+    def __init__(self,outside_r,inside_r):
+        self.outside_c = Circle(outside_r)
+        self.inside_c = Circle(inside_r)
+
+    def area(self):
+        return self.outside_c.area() - self.inside_c.area()
+
+    def perimeter(self):
+        return self.outside_c.perimeter() + self.inside_c.perimeter()
+
+class Birthday:
+    def __init__(self,year,month,day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+class Teacher:
+    def __init__(self,name,age,sex,birthday):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.birthday = birthday
+        self.course = Course(self,'php')
+
+class Course():
+    def __init__(self,teacher,lession):
+        self.teacher = teacher
+        self.lesson = lession
