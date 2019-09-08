a0d89eb90bc11088e887b0edf390b4e13992f06c
mojianhua <1657210793@qq.com>
2019-09-08 23:54:16 +0800

    orm，集合查询和分组查询

--- newStudyPython/DjStudy/DJ02/orm.py
+++ newStudyPython/DjStudy/DJ02/orm.py
@@ -62,19 +62,91 @@ if __name__ == '__main__':
     # ret = models.Author.objects.filter(id__range=[1,3])
     # print(ret)
 
-    print("正向查询".center(80,'*'))
-    book_obj = models.Book.objects.all().first()
-    # 查询出版社名
-    ret = book_obj.pid.name
-    print(ret)
-    # 查询出版社名
-    ret = models.Book.objects.filter(id = 1).values("pid__name")
+    # print("正向查询".center(80,'*'))
+    # book_obj = models.Book.objects.all().first()
+    # # 查询出版社名
+    # ret = book_obj.pid.name
+    # print(ret)
+    # # 查询出版社名
+    # ret = models.Book.objects.filter(id = 1).values("pid__name")
+    # print(ret)
+    #
+    # print("反向查询".center(80, '*'))
+    # publisher_obj = models.Publisher.objects.first()
+    # ret = publisher_obj.back_book.all()
+    # print(ret)
+    #
+    # ret = models.Publisher.objects.filter(id = 8).values("back_book__title")
+    # print(ret)
+
+
+    # 多对多
+    author_obj = models.Author.objects.first()
+    print(author_obj.name)
+    # 查询jim1写过的书
+    ret = author_obj.book.all()
     print(ret)
 
-    print("反向查询".center(80, '*'))
-    publisher_obj = models.Publisher.objects.first()
-    ret = publisher_obj.back_book.all()
-    print(ret)
+    # 1.create，在jim1里面在添加一本书
+    # 通过作者创建一本书，会自动保存
+    # 做了两件事
+    # 1.在book里创建一本新书，2、在作者和书的关系中新建一个关联记录
+    # 注意对应book表里面的字段，title等于title字段，pid_id 等于 pid字段，不是跟model.py对应的
+    # ret = author_obj.book.create(title="书本12",pid_id = 6)
+    # print(ret)
 
-    ret = models.Publisher.objects.filter(id = 8).values("back_book__title")
-    print(ret)
+    # add，在jim1里面在添加一本书
+    # 先拿到id是12的书
+    # book_obj = models.Book.objects.get(id = 10)
+    # ret = author_obj.book.add(book_obj)
+    # print(ret)
+
+    # 添加多个
+    # book_objs = models.Book.objects.filter(id__gt=2)
+    # 要把列表打散再传进去
+    # ret = author_obj.book.add(*book_objs)
+    # print(ret)
+
+    # 直接添加书本id是5的数据
+    # ret = author_obj.book.add(5)
+    # print(ret)
+
+    # # remove
+    # book_obj = models.Book.objects.get(id = 2)
+    # ret = author_obj.book.remove(book_obj)
+    # print(ret)
+    #
+    # # 将书ID 是 3 的从 属于 jim1的记录删除
+    # author_obj.book.remove(3)
+    #
+    # # clear，清空，把jim2打入冰窖，意思说，将作者为jim2从所有书中删除，不留名
+    # jim2 = models.Author.objects.get(id = 2)
+    # jim2.book.clear()
+    #
+    # # 额外补充，外键的反向操作
+    # # 找到id是1的出版社
+    # publisher_obj = models.Publisher.objects.get(id = 1)
+    # publisher_obj.back_book.clear()
+
+    from django.db.models import Avg,Sum,Max,Min,Count
+    # # 计算书本平均价格
+    # ret = models.Book.objects.all().aggregate(price_avg = Avg('price'),price_sum = Sum('price'))
+    # print(ret)
+    #
+    # # 分组查询
+    # # 查询每一本书的作者个数
+    # ret = models.Book.objects.all().annotate(author_num=Count("author"))
+    # for book in ret:
+    #     print("书名：{},作者数:{}".format(book.title,book.author_num))
+    #
+    # # 分组查询数量大于1的数据
+    # ret = models.Book.objects.all().annotate(author_num=Count('author')).filter(author_num__gt=1)
+    # for book in ret:
+    #     print('书名：{}，数量：{}'.format(book.title,book.author_num))
+
+
+    # 每个作者出书的总价
+    ret = models.Author.objects.all().annotate(price_sum=Sum("book__price"))
+    for i in ret:
+        print("作者：{},总价：{}".format(i.name,i.price_sum))
+    print(ret.values_list("name","price_sum"))
