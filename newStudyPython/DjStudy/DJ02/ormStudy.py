790defc0f97a0920ec4afc3e6de0bb265a27f118
mojianhua <1538964080@qq.com>
2019-09-10 11:32:22 +0800

    ORM复习

---
+++ newStudyPython/DjStudy/DJ02/ormStudy.py
@@ -0,0 +1,48 @@
+'''
+orm 总结
+在一个python文件里面加载Django项目
+'''
+import os
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DJ02.settings")
+    import django
+    django.setup()
+    # 第三多对多弄表方式
+    from app03 import models as App03Model
+    ret = App03Model.Author.objects.get(id = 1).bookManyToMany.all()
+    print(ret)
+    # 查询包含2书名的数据，区分大小姐
+    ret = App03Model.Book.objects.filter(title__contains="2")
+    print(ret)
+    # # 查询出版日期是2018的数据库
+    # ret = App03Model.Book.objects.filter(publish_date__year==2018)
+
+    # 查询价格大于10的书
+    ret = App03Model.Book.objects.filter(price__gt=10)
+    print(ret)
+
+    # 查询出版社是广州的数据
+    ret = App03Model.Publisher.objects.filter(addr="广州")
+    print(ret)
+
+    # distinct
+    # 查所有书的关联的出版社
+    ret = App03Model.Book.objects.all().values_list("pid__name")
+    # 去重
+    print(ret.distinct())
+
+    # 排序,desc排序
+    ret = App03Model.Book.objects.all().order_by("price")
+    print(ret)
+
+    # 排序,asc排序
+    ret = App03Model.Book.objects.all().order_by("-price")
+    print(ret)
+
+    # 查询书名是：书222,出版社地址
+    ret = App03Model.Book.objects.filter(title="书222").values("pid__addr")
+    print(ret)
+
+    # 查询书名是：书222,作者的爱好，垮了2张表
+    ret = App03Model.Book.objects.filter(title="书222").values("author__detail__hobby")
+    print(ret)
