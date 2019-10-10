3017b7a2281772995795b401951313beadb8cda5
mojianhua <1538964080@qq.com>
2019-10-10 10:14:02 +0800

    注意事项更新

--- newStudyPython/DjStudy/Djframework/api/utils/Serializer/pagerSerializer.py
+++ newStudyPython/DjStudy/Djframework/api/utils/Serializer/pagerSerializer.py
@@ -3,5 +3,5 @@ from api02 import models as Api02Models
 
 class pagerSerializers(serializers.ModelSerializer):
     class Meta:
-        models = Api02Models.Role
+        model = Api02Models.Role
         fields = '__all__'
