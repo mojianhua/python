a173a5f7b8c47428b00ee5777a613d0a583c8b4e
mojianhua <1538964080@qq.com>
2019-11-01 15:32:32 +0800

    异步报错邮件

--- newStudyPython/DjStudy/Djframework/rabbitmq/CeleryStudyDjango/celery4Task/emailTask.py
+++ newStudyPython/DjStudy/Djframework/rabbitmq/CeleryStudyDjango/celery4Task/emailTask.py
@@ -7,6 +7,8 @@ from email.header import Header
 from rabbitmq.CeleryStudyDjango.Celery4config import app
 import json
 import time
+import Djframework.settings
+import os
 
 class emailTask(object):
     @app.task(bind=True,ignore_result=True)
@@ -26,22 +28,53 @@ class emailTask(object):
         :param port:端口号
         :return
     '''
-    def send_mail(self, recv, title, content):
-        user = 'alert@corp.food2china.com'
+    def send_mail_test(self, recv, title, content):
+        user = 'xxxxxx'
         host = 'smtp.qiye.163.com'
         port = 465
-        password = 'F2c_2302'
+        password = 'xxxxxx'
         msg = MIMEMultipart('alternative')
         msg.attach(MIMEText(content, 'html', 'utf-8'))
         msg['Subject'] = Header(title, 'utf-8')
         msg['From'] = Header(user, 'utf-8')
         msg['To'] = Header(recv, 'utf-8')
-
         try:
             smtp = smtplib.SMTP_SSL(host, port=port)
             smtp.login(user, password)
             smtp.sendmail(user, [recv], msg.as_string())
         except Exception as e:
+            return (str(e))
+        else:
+            return 'SUCCESS'
+
+    '''
+        :param 发送邮件
+        :param recv:邮件接收人地址，多个账号以逗号隔开
+        :param title:邮件标题
+        :param content:邮件内容
+        :param mail_host:邮件服务器
+        :param port:端口号
+        :return
+    '''
+    def send_mail(self, recv, title, content):
+        user = 'xxxx'
+        host = 'smtp.qiye.163.com'
+        port = 465
+        password = 'xxxxxxx'
+        msg = MIMEMultipart('alternative')
+        msg.attach(MIMEText(content, 'html', 'utf-8'))
+        msg['Subject'] = Header(title, 'utf-8')
+        msg['From'] = Header(user, 'utf-8')
+        msg['To'] = Header(recv, 'utf-8')
+        try:
+            smtp = smtplib.SMTP_SSL(host, port=port)
+            smtp.login(user, password)
+            smtp.sendmail(port, [recv], msg.as_string())
+        except Exception as e:
+            # 报错邮件
+            data = {'email': '1657210793@qq.com', 'title': 'Hi World Jim Title', 'content': 'Hi World Jim Content'}
+            path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            emailTask.send_mail_test(self, data['email'], data['title'], (str(e) + 'line：' + path))
             return (str(e))
         else:
             return 'SUCCESS'
