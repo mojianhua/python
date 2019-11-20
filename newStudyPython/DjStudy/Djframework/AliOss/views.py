6219b81ab6a7c719a50f8e956f96ea556c8c6404
mojianhua <1657210793@qq.com>
2019-11-21 00:15:09 +0800

    HAO

--- newStudyPython/DjStudy/Djframework/AliOss/views.py
+++ newStudyPython/DjStudy/Djframework/AliOss/views.py
@@ -11,7 +11,7 @@ class AliOssTest(APIView):
     throttle_classes = []
     def post(self, request, *args, **kwargs):
         # 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        auth = oss2.Auth('LTAIhHrPbeHwidYD', 'YCYEH66bS8KchPCl9suW40CHMeZlKl')
+        auth = oss2.Auth('AK', 'SK')
         # Endpoint以杭州为例，其它Region请按实际情况填写。
         bucket = oss2.Bucket(auth, 'https://pic.food2china.cn', 'f2c-pic')
         print(bucket)
